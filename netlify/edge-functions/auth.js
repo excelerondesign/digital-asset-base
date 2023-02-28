7b4dc67f7d2c5ffc260e47c59145e7bc489c9d95
Connor Wilson <connor.wilson@excelerondesigns.com>
2023-02-28 16:51:21 -0500

    Another attempt at getting edge functions to work

--- netlify/edge-functions/auth.js
+++ netlify/edge-functions/auth.js
@@ -2,22 +2,33 @@
 /**
  * @param {Request} req
  */
+/*
+// @NOTE: Man who knows,,, at this point I'm starting to think it might not be possible
 export default async (req, _ctx) => {
-    //console.log('something');
+    const clone = req.clone()
+    const url = new URL(req.url);
+    console.log('something');
+    console.log(await _ctx.cookeis.get('authed'));
+
     // console.log(req);
     // const url = await fetch(req.url);
     if (_ctx.cookies.get('authed')) {
-        return Response.redirect(req.url);
+        console.log(clone.headers.get('host'));
+        return Response.redirect(url);
     }
-    console.log(req.referrer, _ctx )
+    
+    return _ctx.next();
+    // const host = req.headers.get('host');
+    // console.log(req.headers.get('host'), _ctx,next)
     // @TODO _ctx.site.url goes to the *actual* url not the local dev url, gotta
     // figure how to get the local host
     //
     // Using req.url or req.host or req.referrer ends up being undefined despite
     // being console-able
-    return Response.redirect(req.referrer + '/auth');
-}
+    // return Response.redirect(new URL('/auth'));
+}*/
 
-export const config = {
-    path: ['/*']
-};
+export default async (req, ctx) => {
+    console.log('is literally anything happening???');
+    return ctx.rewrite('/auth');
+}
